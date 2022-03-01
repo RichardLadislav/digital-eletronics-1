e6619ef95e9d8f899aba8dd7a9029249b50e26ec
student <student@PC-636.urel.feec.vutbr.cz>
2022-03-01 14:48:21 +0100

    mux a com

---
+++ labs/03-vivado/multiplexer/multiplexer.srcs/sim_1/new/tb_multiplexer.vhd
@@ -0,0 +1,43 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 03/01/2022 02:33:40 PM
+-- Design Name: 
+-- Module Name: tb_multiplexer - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity tb_multiplexer is
+--  Port ( );
+end tb_multiplexer;
+
+architecture Behavioral of tb_multiplexer is
+
+begin
+
+
+end Behavioral;
